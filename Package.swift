49361306b3a9801afb424e44d0170b11cd45591e
Colin Tremblay <colin@withuno.com>
2021-02-23 15:31:09 -0700

    clean

--- Package.swift
+++ Package.swift
@@ -2,36 +2,6 @@
 
 import PackageDescription
 
-/*
-let package = Package(
-    name: "CArgon2",
-    products: [
-        .library(
-            name: "CArgon2",
-            targets: ["CArgon2"]
-        ),
-    ],
-    targets: [
-        .target(
-            name: "CArgon2",
-            path: "src",
-            exclude: [
-                "blake2/blamka-round-opt.h",
-                "bench.c",
-                "genkat.c",
-                "genkat.h",
-                "opt.c",
-                "run.c",
-                "test.c"
-            ],
-            publicHeadersPath: "include",
-            cSettings: [
-                .headerSearchPath("../include")
-            ]
-        )
-    ]
-)*/
-
 let package = Package(
     name: "argon2",
     products: [
